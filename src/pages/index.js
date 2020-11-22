44677cb5fe4031816944b6b8406220f9114de15d
幸村昌裕 <koumura@yukimuramasahironoMacBook-Pro.local>
2020-11-22 18:46:14 +0900

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,14 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Learn to design and code React apps</h1>
+    <p>Complete cources about the best tools and design systems. Ptototype and build apps with React and Swift.</p>
     <p>Now go build something great.</p>
+
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
